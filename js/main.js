a2ceee3ad8036b15d2b5f4f6ae3cb1aeefda79d7
ohyoonah <dhdbsdk3@naver.com>
2022-07-21 17:57:34 +0900

    메뉴 smooth 스크롤 효과주기

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 로딩화면 구현
 const welcome = document.getElementById('welcome');
 const up = document.getElementById('up');
 const header = document.querySelector('header');
@@ -14,3 +15,21 @@ setTimeout(() => {
     {opacity: 0}
   ], 3000);
 }, 2000)
+
+
+// 메뉴 스크롤 효과
+var menu = document.querySelectorAll('nav a');
+
+function scroll(e) {
+  e.preventDefault();
+  var target = document.querySelector(this.getAttribute('href'));
+
+  window.scrollTo ({
+      'behavior': 'smooth',
+      'top': target.offsetTop,
+  });
+}
+
+for(var i = 0; i < menu.length; i ++) {
+    menu[i].onclick = scroll;
+}
